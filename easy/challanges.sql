8c7ca887b334376609ce61c83e715bdabb774eb8
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-04 10:10:44 +0100

    add sql script 4

--- easy/challanges.sql
+++ easy/challanges.sql
@@ -9,3 +9,6 @@ AND CountryCode = 'USA';
 
 -- Query the NAME field for all American cities in the CITY table with populations larger than 120000. The CountryCode for America is USA.
 SELECT NAME FROM CITY WHERE POPULATION > 120000 AND COUNTRYCODE = 'USA';
+
+-- Query all columns for a city in CITY with the ID 1661.
+SELECT * FROM CITY WHERE ID = 1661;
