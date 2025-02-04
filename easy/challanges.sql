4cb4f190d90a2494d25994f649d496a6c1414449
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-04 10:37:00 +0100

    add sql script 7

--- easy/challanges.sql
+++ easy/challanges.sql
@@ -21,3 +21,6 @@ SELECT NAME FROM CITY WHERE COUNTRYCODE = 'JPN';
 
 -- Query a list of CITY and STATE from the STATION table.
 Query a list of CITY and STATE from the STATION table.
+
+-- Query a list of CITY names from STATION for cities that have an even ID number. Print the results in any order, but exclude duplicates from the answer.
+SELECT DISTINCT CITY  FROM STATION  WHERE MOD(ID,2) = 0; -- The SELECT DISTINCT statement is used to return only distinct (different) values.
