2db8f32051a0dba6fcdcffd0e313bd4876f591bb
findbyid1 <sanaaennaji93@gmail.com>
2025-07-01 17:32:16 +0200

    query list sql

--- easy/challanges.sql
+++ easy/challanges.sql
@@ -24,3 +24,9 @@ Query a list of CITY and STATE from the STATION table.
 
 -- Query a list of CITY names from STATION for cities that have an even ID number. Print the results in any order, but exclude duplicates from the answer.
 SELECT DISTINCT CITY  FROM STATION  WHERE MOD(ID,2) = 0; -- The SELECT DISTINCT statement is used to return only distinct (different) values.
+
+-- Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.
+select distinct city
+from station
+where not (city like 'A%' or city like 'E%' or city like 'I%' or city like 'O%' or city like 'U%');
+
