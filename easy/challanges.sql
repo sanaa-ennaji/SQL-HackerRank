9cbed59a5d71a73008a1c4f09e697fac7a533b90
findbyid1 <sanaaennaji93@gmail.com>
2025-07-07 16:18:01 +0200

    students marks

--- easy/challanges.sql
+++ easy/challanges.sql
@@ -60,8 +60,13 @@ WHERE CITY NOT LIKE 'A%'
   AND CITY NOT LIKE '%i'
   AND CITY NOT LIKE '%o'
   AND CITY NOT LIKE '%u';
-
-
+-- Query the Name of any student in STUDENTS who scored higher than  Marks.
+-- Order your output by the last three characters of each name. 
+--If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
+SELECT Name
+FROM STUDENTS
+WHERE Marks > 75
+ORDER BY RIGHT(Name, 3), ID;
 
 
 
