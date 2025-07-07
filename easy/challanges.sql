76d762ef7019c5d106f55b3a124aef43e5055e87
findbyid1 <sanaaennaji93@gmail.com>
2025-07-07 11:52:48 +0200

    start with vowels and do not end with vowels.

--- easy/challanges.sql
+++ easy/challanges.sql
@@ -45,7 +45,23 @@ AND CITY NOT LIKE '%u';
 SELECT DISTINCT CITY
 FROM STATION
 WHERE LOWER(SUBSTR(CITY,1,1)) NOT IN ('a','e','i','o','u')
-   OR LOWER(SUBSTR(CITY,-1,1)) NOT IN ('a','e','i','o','u');
+OR LOWER(SUBSTR(CITY,-1,1)) NOT IN ('a','e','i','o','u');
+
+-- Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. Your result cannot contain duplicates.
+SELECT DISTINCT CITY 
+FROM STATION
+WHERE CITY NOT LIKE 'A%' 
+  AND CITY NOT LIKE 'E%'
+  AND CITY NOT LIKE 'I%'
+  AND CITY NOT LIKE 'O%'
+  AND CITY NOT LIKE 'U%'
+  AND CITY NOT LIKE '%a'
+  AND CITY NOT LIKE '%e'
+  AND CITY NOT LIKE '%i'
+  AND CITY NOT LIKE '%o'
+  AND CITY NOT LIKE '%u';
+
+
 
 
 
