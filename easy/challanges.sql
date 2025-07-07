6fb568e91d524f6e11dbc0be1190ada5060932a0
findbyid1 <sanaaennaji93@gmail.com>
2025-07-07 18:10:54 +0200

    LIST OF EMPOLOEE

--- easy/challanges.sql
+++ easy/challanges.sql
@@ -71,4 +71,7 @@ ORDER BY RIGHT(Name, 3), ID;
 SELECT NAME FROM EMPLOYEE 
 ORDER BY NAME;
 
-
+-- Write a query that prints a list of employee names (i.e.: the name attribute) for employees in Employee having a salary greater than  per month who have been employees for less than  months. Sort your result by ascending employee_id.
+SELECT NAME FROM EMPLOYEE 
+WHERE SALARY > 2000 AND MONTHS < 10 
+ORDER BY  employee_id ASC;
