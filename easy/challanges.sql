09d554cb60127d4181b93d94657474ef55f59ba6
findbyid1 <sanaaennaji93@gmail.com>
2025-07-09 10:42:22 +0200

    ccount

--- easy/challanges.sql
+++ easy/challanges.sql
@@ -84,3 +84,7 @@ CASE
      WHEN A !=  B AND B !=  C THEN 'Scalene'
 END
 FROM TRIANGLES; 
+
+-- Query a count of the number of cities in CITY having a Population larger than .
+SELECT COUNT(*) FROM CITY 
+WHERE POPULATION > 100000;
