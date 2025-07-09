48f3fff710e99a70c349db80346a2922ec5f0fba
findbyid1 <sanaaennaji93@gmail.com>
2025-07-09 10:58:42 +0200

    SUM

--- easy/challanges.sql
+++ easy/challanges.sql
@@ -88,3 +88,5 @@ FROM TRIANGLES;
 -- Query a count of the number of cities in CITY having a Population larger than .
 SELECT COUNT(*) FROM CITY 
 WHERE POPULATION > 100000;
+--Query the total population of all cities in CITY where District is California.
+SELECT SUM(POPULATION) FROM CITY WHERE DISTRICT='California';
