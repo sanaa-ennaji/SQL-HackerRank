01d7cab76d68d4cffa79bb20ba8dabdc65e7c5bf
findbyid1 <sanaaennaji93@gmail.com>
2025-07-09 11:17:01 +0200

    round

--- easy/challanges.sql
+++ easy/challanges.sql
@@ -86,7 +86,10 @@ END
 FROM TRIANGLES; 
 
 -- Query a count of the number of cities in CITY having a Population larger than .
-SELECT COUNT(*) FROM CITY 
-WHERE POPULATION > 100000;
+SELECT COUNT(*) FROM CITY WHERE POPULATION > 100000;
 --Query the total population of all cities in CITY where District is California.
 SELECT SUM(POPULATION) FROM CITY WHERE DISTRICT='California';
+-- Query the average population of all cities in CITY where District is California.
+SELECT AVG(POPULATION) FROM CITY WHERE DISTRICT='California';
+-- Query the average population for all cities in CITY, rounded down to the nearest integer.
+SELECT ROUND(AVG(POPULATION)) FROM CITY ;
