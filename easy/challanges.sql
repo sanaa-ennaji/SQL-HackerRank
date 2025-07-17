45b5b58e61f3280fc5e8e83516d4e2bafb081a46
findbyid1 <sanaaennaji93@gmail.com>
2025-07-17 15:55:56 +0200

    group by

--- easy/challanges.sql
+++ easy/challanges.sql
@@ -145,3 +145,9 @@ AND b.CONTINENT='asia';
 SELECT a.NAME FROM CITY a , COUNTRY b 
 WHERE a.COUNTRYCODE = b.CODE 
 AND b.CONTINENT='africa';
+
+-- Given the CITY and COUNTRY tables, query the names of all the continents (COUNTRY.Continent) and their respective average city populations (CITY.Population) rounded down to the nearest integer.
+SELECT COUNTRY.CONTINENT, FLOOR(AVG(CITY.POPULATION))
+FROM CITY INNER JOIN COUNTRY
+ON CITY.COUNTRYCODE = COUNTRY.CODE
+GROUP BY COUNTRY.CONTINENT;
