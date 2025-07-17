76688abe8b65a3716c4f3e8d2defe5c36cd4aca8
findbyid1 <sanaaennaji93@gmail.com>
2025-07-17 16:14:00 +0200

    Weather Observation Station 18

--- medium/challanges.sql
+++ medium/challanges.sql
@@ -7,3 +7,5 @@ lower(occupation), 's.') as profession
 from occupations
 group by occupation
 order by profession;
+-- Weather Observation Station 18 
+select ROUND(ABS(MAX(LAT_N) - MIN(LAT_N)) + ABS(MAX(LONG_W) - MIN(LONG_W)), 4) FROM STATION; -- MYSQL
